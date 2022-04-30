baaa1a5362f73a4955be52846394705287605ec8
Hunter <43457196+hjnauman@users.noreply.github.com>
2022-04-30 16:02:57 -0500

    Update node_sniff.py

--- node_sniff.py
+++ node_sniff.py
@@ -35,7 +35,8 @@ def pkt_handler(pkt: Packet):
         rssi_val = pkt[RadioTap].dBm_AntSignal
 
         payload = f'{rpi_node}{DELIM}{time_sniffed}{DELIM}{mac_address}{DELIM}{ssid}{DELIM}{organization}{DELIM}{rssi_val}'
-        print(pkt.payload.name)
+        dot11_layer = pkt.getlayer(Dot11)
+        print(dot11_layer.payload.name)
         # print(pkt.show())
         # print('\n\n\n')
 
