b66ed0cba3c98c9b666b2fe53d6cc02ef9ad77cf
Hunter <43457196+hjnauman@users.noreply.github.com>
2022-05-01 22:05:03 -0500

    adding some constants for RPI location testing, and printing mac address for testing

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
 TOPIC = 'uark/csce5013/hjnauman/wifi_rtls'
 DELIM = ' | '
 MQTT_HOSTNAME = 'broker.hivemq.com'
+
+RPI_0_COORDINATES = {'X': 0, 'Y': 0}
+RPI_1_COORDINATES = {'X': 272, 'Y': 348}
+RPI_2_COORDINATES = {'X': 408, 'Y': 0}
--- node_sniff.py
+++ node_sniff.py
@@ -35,6 +35,7 @@ def pkt_handler(pkt: Packet):
         rssi_val = pkt[RadioTap].dBm_AntSignal
 
         payload = f'{rpi_node}{DELIM}{time_sniffed}{DELIM}{mac_address}{DELIM}{ssid}{DELIM}{organization}{DELIM}{rssi_val}'
+        print(mac_address)
 
         publish.single(TOPIC, payload, hostname=MQTT_HOSTNAME, port=1883, keepalive=60)
 
